0db3a4e944e53479f246606445155c7a76dfec5f
nplutzik <nplutzik@gmail.com>
2014-04-25 10:40:32 -0400

    McKennethsMemes

---
+++ McKenneth_Memes/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+McKennethMemes::Application.config.secret_key_base = '23d39aa5e34d8bd630fc5933fb8bdd5ec34c3569d78e09072e439e4b963ed2013ba0e45320620c6f164ba6a0fb77b545924dee24faff8e36b827679810116f3e'
---
+++ McKenneth_Memes/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+McKennethMemes::Application.config.session_store :cookie_store, key: '_McKenneth_Memes_session'
